2680cf9eab2fbf7c219a374c84390f4229af5786
ssnikolay <ssnikolay@gmail.com>
2015-01-28 18:58:53 +0300

    simple sequence :grean_heart:

---
+++ simple_sequence.rb
@@ -0,0 +1,40 @@
+class SimpleSequence
+  attr_reader :sequence
+
+  def initialize(count = 100)
+    @count = count
+  end
+
+  def generate
+    @sequence = %w(1)
+
+    (@count - 1).times do
+      new_item = get_item_by(@sequence.last)
+      @sequence.push(new_item)
+      puts new_item
+    end
+  end
+
+  def print
+
+  end
+
+  private
+
+  def get_item_by(el)
+    number = el[0]
+    count = 1
+
+    String.new.tap do |str|
+      el.length.times do |i|
+        if el[ i + 1 ] == number
+          count += 1
+        else
+          str << "#{ count }#{ number }"
+          count = 1
+          number = el[ i + 1 ]
+        end
+      end
+    end
+  end
+end
