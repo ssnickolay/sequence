08937443a609eae157f7ebff55a1ac7220ff0848
ssnikolay <ssnikolay@gmail.com>
2015-01-30 14:11:40 +0300

    add comments

--- sequence/magic_sequence.rb
+++ sequence/magic_sequence.rb
@@ -35,43 +35,62 @@ module Sequence
         [].tap do |arr|
         while i < el.count
           if @current_repeats.any?
+            # Если есть повторения, то обрабатываем их
             repeat_index = @current_repeats.shift
 
             if repeat_index >= i
 
               repeat_el = el[repeat_index]
 
+              # Обрабатываем интервал от [i .. repeat_index] - там нет повторений
               arr.concat(only_units(el, arr, i, i = repeat_index)) if i != repeat_index
+
+              # Ищем количество повторов
               count = find_repeat_count(el, i, repeat_el)
 
+              # Если количество поторов == последний элемент в текущем результате, то добавляем в массив повторов
               @next_repeats << arr.count - 1 if arr.last == count.to_s
+
+              # Добавляем в результирующий массив
               arr.concat([ count.to_s, repeat_el ])
 
+              # Если количество повторов == повторяющийся элемент
               @next_repeats << arr.count - 2 if repeat_el == count.to_s
+
+              # Если повторяющийся элемент еденица
               @next_units   << arr.count - 1 if repeat_el == '1'
 
               i += count
             end
           else
+            # Обрабатываем "хвост", там не повторов
             arr.concat(only_units(el, arr, i, i = el.count))
           end
         end
       end
     end
 
+    # [2, 3, 4, 1] => [1, 2, 1, 3, 1, 4, 1, 1]
     def only_units(el, new_el, i_current, i_next)
-      # [2, 3, 4, 1] => [1, 2, 1, 3, 1, 4, 1, 1]
+      # Кусок в котором нет повторов
       joined_el = el[i_current ... i_next]
 
+      # Новый кусок с еденицами м\у элементов
       result = "1#{ joined_el.join('1') }".split('')
+
+      # Узнаем есть ли в текущем куске joined_el еденицы => вернет массив координат
       repeat_units = @current_units.select{ |u| u < i_next && u >= i_current } # значит в куске el есть единицы
 
+      # Поскольку в result первая стоит 1, то есть вариант, что в текущем результате последняя == 1, значит повторо
       @next_repeats << new_el.count - 1 if new_el.last == '1'
 
+      # Если нашли еденицы в joined_el, то конвертируем координаты из системы счисления старого елемента (el), в новый (new_el)
       if repeat_units.any?
-        @next_repeats.concat(repeat_units.map{ |u| new_el.count +  (u - i_current) * 2 }) # NEW_EL_COUNT + (index - (ALL_COUNT - EL_COUNT)) * 2 # сдвиг из-за едениц
+        @next_repeats.concat(repeat_units.map{ |u| new_el.count +  (u - i_current) * 2 })
       end
-      @next_units.concat( joined_el.count.times.collect{ |u| new_el.count + (u * 2) } ) # на каждое 2ое место встанет 1
+
+      # Добавляем в массив едениц нечетные координаты joined_el, потому что там еденицы
+      @next_units.concat( joined_el.count.times.collect{ |u| new_el.count + (u * 2) } )
 
       result
     end
